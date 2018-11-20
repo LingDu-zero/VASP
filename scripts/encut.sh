818bfd2e53eb70cb179e83878a9b8a335887a715
LingDu-zero <33028145+LingDu-zero@users.noreply.github.com>
2018-11-20 22:09:57 +0800

    Update encut.sh

--- scripts/encut.sh
+++ scripts/encut.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 for i in `seq 300 50 700`
 do
-cat > INCAR << !
+cat > INCAR <<!
 SYSTEM = Si
-PREC = Accurate
-KSPACING = 0.2
 
-IBRION = 1
-ISIF = 2
+ISTART = 0
+ICHARG = 2
 
-ISMEAR = -5
-NSW = 0
+ISMEAR = 0
+SIGMA = 0.05
 
-EDIFF = 1e-5
 ENCUT = $i
 
 LWAVE = .FALSE.
 LCHARGE = .FALSE.
 !
 
-echo 'ENCUT = $i eV'
+echo "ENCUT=$i eV"
 EXE=/THFS/opt/vasp/5.4.4/vasp.5.4.4/bin/vasp_std
 yhrun  -n 4 -p TYUT  $EXE
 
 E=`grep "TOTEN" OUTCAR | tail -1 | awk '{printf "%12.6f \n", $5 }'`
-echo $i $E >> encut.dat
+echo $i $E>> encut.dat
 done
